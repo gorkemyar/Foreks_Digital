356a26a366ead50db99f94abb94a629caf8670f3
gorkem yar <gorkemyar@sabanciuniv.edu>
2023-09-01 15:00:24 +0300

    folder restructure

---
+++ Foreks_Digital/Model/SegmentModel.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+
+struct Segment{
+    var key: String
+    var search: [Stock]
+}
---
+++ Foreks_Digital/Model/StockModel.swift
@@ -0,0 +1,40 @@
+import Foundation
+
+struct Stock: Codable, Identifiable, Hashable{
+    var id: String {
+        return tke
+    }
+    let gro: String
+    let cod: String
+    let tke: String
+    let def: String
+    
+    func copy() -> Stock{
+        return Stock(gro: gro, cod: cod, tke: tke, def: def)
+    }
+    func toDetailed () -> StockDetailed{
+        var tmp: [String: String] = ["cod": cod, "gro": gro, "tke": tke, "def": def]
+        return StockDetailed(stockDict: tmp)
+    }
+    static func ==(lhs:Stock, rhs:Stock) -> Bool { // Implement Equatable
+        return lhs.id == rhs.id
+    }
+    
+    func hash(into hasher: inout Hasher){
+        hasher.combine(id)
+    }
+}
+
+struct StockDetailed: Codable, Identifiable{
+    var id: String {
+        return stockDict["tke"]!
+    }
+    var stockDict: [String:String]
+    var changePositive: Bool?
+}
+
+
+struct StockDetailedHelper: Codable{
+    let l: [[String:String]]
+    let z: String
+}
